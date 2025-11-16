602057726f7165097033367313dbf727d2a04126
Joemuigai <39778761+Joemuigai@users.noreply.github.com>
2025-11-16 20:58:46 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Joemuigai\LaravelMpesa\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Joemuigai\LaravelMpesa\LaravelMpesaServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
